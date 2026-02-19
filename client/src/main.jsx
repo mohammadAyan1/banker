4b96ad1752006d371b1910b7609cc5d51012c7e4
mohammadAyan1 <mohammadayan2210@gmail.com>
2026-02-19 12:37:23 +0530

    Hash Router Added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
